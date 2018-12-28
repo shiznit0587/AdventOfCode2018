32598d6b2dd5afcc004e5d753d6065e5732f7163
Stachowski <pstachowski@Philips-MBP-EA.home>
2018-12-28 11:07:00 -0500

    Stubbed Day 3

---
+++ src/day3.rs
@@ -0,0 +1,11 @@
+use crate::utils;
+
+pub fn day3() -> std::io::Result<()> {
+    println!("Running Day 3 - a");
+
+    let lines = utils::readDay(3)?;
+
+    println!("Running Day 3 - b");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 
 mod day1;
 mod day2;
+mod day3;
 
 pub mod utils;
 
 fn main() -> std::io::Result<()> {
     day1::day1()?;
     day2::day2()?;
+    day3::day3()?;
 
     Ok(())
 }
