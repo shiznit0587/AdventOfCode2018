157e6b65439e27584ed30379320fbd0a9d98af0d
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2018-12-31 09:13:15 -0500

    Day 4 complete

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,106 @@
+extern crate regex;
+
 use crate::utils;
+use regex::Regex;
+use std::collections::HashMap;
 
 pub fn day4() -> std::io::Result<()> {
     println!("Running Day 4 - a");
 
-    let lines = utils::readDay(4)?;
+    let mut lines = utils::readDay(4)?;
+    lines.sort(); // ... seriously? wow, that was easy.
+
+    let timeRex = Regex::new(r"\d+:(?P<m>\d+)").unwrap();
+    let beginRex = Regex::new(r"Guard #(?P<g>\d+)").unwrap();
+
+    let mut guards: HashMap<i32, Guard> = HashMap::new();
+
+    let mut guardOnDuty: Option<i32> = Option::None;
+    let mut asleep: Option<i32> = Option::None;
+
+    for line in lines.iter() {
+        if line.contains("falls asleep") {
+            let minute = utils::tryParse_i32(&timeRex.captures(line).unwrap()["m"]);
+            asleep = Option::Some(minute);
+        } else if line.contains("wakes up") {
+            let minute = utils::tryParse_i32(&timeRex.captures(line).unwrap()["m"]);
+            let guard = guards.get_mut(&guardOnDuty.unwrap()).unwrap();
+            guard.sleep(asleep.unwrap(), minute);
+        } else {
+            let guardId = utils::tryParse_i32(&beginRex.captures(line).unwrap()["g"]);
+            let mut guard = guards.entry(guardId).or_insert(Guard::new());
+            guard.id = guardId;
+            guardOnDuty = Option::Some(guard.id);
+        }
+    }
+
+    guards.values_mut().for_each(Guard::finalize);
+
+    let sleepiestGuard = guards
+        .values()
+        .max_by(|g1, g2| g1.minsAsleep.cmp(&g2.minsAsleep))
+        .unwrap();
+
+    println!(
+        "Guard # {} :: Mins Asleep = {}, Min Most Asleep = {}, Times Asleep in Min = {}, Checksum = {}",
+        sleepiestGuard.id,
+        sleepiestGuard.minsAsleep,
+        sleepiestGuard.minMost,
+        sleepiestGuard.mostInMin,
+        sleepiestGuard.id * sleepiestGuard.minMost
+    );
 
     println!("Running Day 4 - b");
 
+    let predictableGuard = guards
+        .values()
+        .max_by(|g1, g2| g1.mostInMin.cmp(&g2.mostInMin))
+        .unwrap();
+
+    println!(
+        "Guard # {} :: Mins Asleep = {}, Min Most Asleep = {}, Times Asleep in Min = {}, Checksum = {}",
+        predictableGuard.id,
+        predictableGuard.minsAsleep,
+        predictableGuard.minMost,
+        predictableGuard.mostInMin,
+        predictableGuard.id * predictableGuard.minMost
+    );
+
     Ok(())
+}
+
+struct Guard {
+    id: i32,
+    mins: [i32; 60],
+    minsAsleep: i32,
+    minMost: i32,
+    mostInMin: i32,
+}
+
+impl Guard {
+    fn new() -> Guard {
+        Guard {
+            id: 0,
+            mins: [0; 60],
+            minsAsleep: 0,
+            minMost: -1,
+            mostInMin: -1,
+        }
+    }
+
+    fn sleep(&mut self, from: i32, to: i32) {
+        for i in from..to {
+            self.mins[i as usize] += 1;
+        }
+    }
+
+    fn finalize(&mut self) {
+        self.minsAsleep = self.mins.iter().fold(0, |acc, x| acc + x);
+        for (i, val) in self.mins.iter().enumerate() {
+            if *val > self.mostInMin {
+                self.mostInMin = *val;
+                self.minMost = i as i32;
+            }
+        }
+    }
 }
