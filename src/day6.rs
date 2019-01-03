eab56c585b36862cbc9d514b05575f75c4c7421e
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-02 21:38:35 -0500

    Stubbed Day 6

---
+++ src/day6.rs
@@ -0,0 +1,11 @@
+use crate::utils;
+
+pub fn day6() -> std::io::Result<()> {
+    println!("Running Day 6 - a");
+
+    let lines = utils::readDay(6)?;
+
+    println!("Running Day 6 - b");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 pub mod utils;
 
@@ -15,6 +16,7 @@ fn main() -> std::io::Result<()> {
     day3::day3()?;
     day4::day4()?;
     day5::day5()?;
+    day6::day6()?;
 
     Ok(())
 }
