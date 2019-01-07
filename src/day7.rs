5177beceb1263978e0b1736cb86784202fd1df6a
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-07 09:36:17 -0500

    Refactored Day 7 to a struct & iterator to help in implementing Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -2,64 +2,30 @@ use crate::utils;
 use regex::Regex;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
+use std::fmt;
+
+const NUM_NODES: usize = 26;
+
+type Edge = (usize, usize);
 
 pub fn day7() -> std::io::Result<()> {
     println!("Running Day 7 - a");
 
     let rex = Regex::new(r"Step (\w) .* step (\w)").unwrap();
 
-    let edges: Vec<(usize, usize)> = utils::readDay(7)?
+    let edges: Vec<Edge> = utils::readDay(7)?
         .iter()
         .map(|l| rex.captures(l).unwrap())
-        .map(|c| (utils::tryParse_char(&c[1]), utils::tryParse_char(&c[2])))
-        .map(|(a, b)| ((a - 65) as usize, (b - 65) as usize))
+        .map(|c| (Node::get_id_from_name(&c[1]), Node::get_id_from_name(&c[2])))
         .collect();
 
-    let mut visited: u32 = 0;
-    let mut visitedCount = 0;
-    let mut visitOrder: [usize; 26] = [0; 26];
+    let mut day7 = Day7::new(&edges);
 
-    let mut nodes = Vec::with_capacity(26);
-    for i in 0..26 {
-        nodes.push(Node::new(i));
-    }
-
-    for edge in edges {
-        addNodeDependency(edge.0, edge.1, &mut nodes);
-    }
-
-    let mut queue: BinaryHeap<&Node> = BinaryHeap::new();
-    for node in nodes.iter().filter(|n| n.dependencies == 0) {
-        // println!("Queueing {}", node.get_char());
-        queue.push(node);
-    }
-
-    while visitedCount < 26 {
-        let node = queue.pop().unwrap();
-        // println!("Visiting {}", node.get_char());
-
-        utils::set_bit(&mut visited, node.id);
-        visitOrder[visitedCount] = node.id;
-        visitedCount += 1;
-
-        for i in 0..26 {
-            if utils::is_bit_set(&node.dependents, i) {
-                let nextNode = &nodes[i];
-                // println!("Checking dependent {}...", nextNode.get_char());
-
-                if utils::intersection(nextNode.dependencies, visited) == nextNode.dependencies {
-                    queue.push(nextNode);
-                    // println!("Queueing {}", nextNode.get_char());
-                }
-            }
-        }
-
-        // println!("Finished visiting {}", node.get_char());
-    }
+    let visitOrder: [usize; NUM_NODES] = day7.solve_a();
 
     let order = visitOrder
         .iter()
-        .map(|&i| nodes[i].get_char())
+        .map(|&i| day7.nodes[i].get_name())
         .collect::<String>();
 
     println!("Instruction Order = {}", order);
@@ -69,10 +35,103 @@ pub fn day7() -> std::io::Result<()> {
     Ok(())
 }
 
-fn addNodeDependency(from: usize, to: usize, nodes: &mut Vec<Node>) {
-    // println!("Tracking dependency from {} to {}", nodes[from].get_char(), nodes[to].get_char());
-    utils::set_bit(&mut nodes[from].dependents, to);
-    utils::set_bit(&mut nodes[to].dependencies, from);
+struct Day7 {
+    nodes: Vec<Node>,
+}
+
+impl Day7 {
+    fn new(edges: &Vec<Edge>) -> Day7 {
+        let mut day7 = Day7 {
+            nodes: Vec::with_capacity(NUM_NODES),
+        };
+
+        for i in 0..NUM_NODES {
+            day7.nodes.push(Node::new(i));
+        }
+
+        for edge in edges {
+            utils::set_bit(&mut day7.nodes[edge.0].dependents, edge.1);
+            utils::set_bit(&mut day7.nodes[edge.1].dependencies, edge.0);
+        }
+
+        day7
+    }
+
+    fn solve_a(&mut self) -> [usize; NUM_NODES] {
+        let mut visitOrder: [usize; NUM_NODES] = [0; NUM_NODES];
+        let mut visitCount = 0;
+        let mut iter = self.iter();
+
+        while let Some(node) = iter.next() {
+            iter.visit(node);
+            visitOrder[visitCount] = node.id;
+            visitCount += 1;
+            // println!("Finished visiting {:?}", node);
+        }
+
+        visitOrder
+    }
+
+    fn iter(&self) -> Day7Iterator<'_> {
+        Day7Iterator::new(self)
+    }
+}
+
+struct Day7Iterator<'a> {
+    day7: &'a Day7,
+    visited: u32,
+    queue: BinaryHeap<&'a Node>,
+}
+
+impl Day7Iterator<'_> {
+    fn new(day7: &Day7) -> Day7Iterator {
+        let mut iterator = Day7Iterator {
+            day7: day7,
+            visited: 0,
+            queue: BinaryHeap::with_capacity(NUM_NODES),
+        };
+
+        for node in day7.nodes.iter().filter(|n| n.dependencies == 0) {
+            // println!("Queueing {:?}", node);
+            iterator.queue.push(node);
+        }
+
+        iterator
+    }
+
+    fn visit(&mut self, node: &Node) {
+        // println!("Visiting {:?}", node);
+
+        utils::set_bit(&mut self.visited, node.id);
+
+        for i in 0..NUM_NODES {
+            if utils::is_bit_set(&node.dependents, i) {
+                let nextNode = &self.day7.nodes[i];
+                // println!("Checking dependent {:?}...", nextNode);
+
+                if utils::intersection(nextNode.dependencies, self.visited) == nextNode.dependencies
+                {
+                    // println!("Queueing {:?}", nextNode);
+                    self.queue.push(nextNode);
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Iterator for Day7Iterator<'a> {
+    type Item = &'a Node;
+
+    fn next(&mut self) -> Option<&'a Node> {
+        let next = self.queue.pop();
+        // match next {
+        //     Some(node) => {
+        //         println!("Iterator :: next={:?}", node);
+        //     }
+        //     _ => {}
+        // }
+        next
+    }
 }
 
 #[derive(Eq, PartialEq)]
@@ -91,8 +150,18 @@ impl Node {
         }
     }
 
-    fn get_char(&self) -> char {
-        (self.id as u8 + 65) as char
+    fn get_name(&self) -> String {
+        ((self.id as u8 + 65) as char).to_string()
+    }
+
+    fn get_id_from_name(name: &str) -> usize {
+        (utils::tryParse_char(name) - 65) as usize
+    }
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.get_name())
     }
 }
 
