85dbea4c6e259a2efe6330e17f6dc182d17615ac
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-12 12:30:51 -0500

    Finished Day 8 Part 2

--- src/day8.rs
+++ src/day8.rs
@@ -10,34 +10,42 @@ pub fn day8(lines: &mut Vec<String>) {
         .map(|s| utils::parse(s))
         .collect::<Vec<u32>>();
 
-    let license = calc_license(&code);
+    let licenses = calc_licenses(&code);
 
-    println!("License = {}", license);
+    println!("License = {}", licenses.0);
 
     println!("Running Day 8 - b");
+
+    println!("License = {}", licenses.1);
 }
 
-fn calc_license(code: &Vec<u32>) -> u32 {
+fn calc_licenses(code: &Vec<u32>) -> (u32, u32) {
     let mut cursor = 0;
-    let mut license = 0;
-    visit_node(code, &mut cursor, &mut license);
-    license
+    let mut license_a = 0;
+    let license_b = visit_node(code, &mut cursor, &mut license_a);
+    (license_a, license_b)
 }
 
-fn visit_node(code: &Vec<u32>, cursor: &mut usize, license: &mut u32) {
-    if *cursor + 1 >= code.len() {
-        return;
-    }
-
+fn visit_node(code: &Vec<u32>, cursor: &mut usize, license_a: &mut u32) -> u32 {
     let header = (code[*cursor] as usize, code[*cursor + 1] as usize);
     *cursor += 2;
 
+    let mut child_values: Vec<u32> = Vec::with_capacity(header.0);
     for _ in 0..header.0 {
-        visit_node(code, cursor, license);
+        child_values.push(visit_node(code, cursor, license_a));
     }
 
+    let mut value = 0;
     for _ in 0..header.1 {
-        *license += code[*cursor];
+        let meta = code[*cursor];
+        if header.0 == 0 {
+            value += meta;
+        } else if 0 < meta && meta <= child_values.len() as u32 {
+            value += child_values[(meta - 1) as usize];
+        }
+        *license_a += meta;
         *cursor += 1;
     }
+
+    value
 }
