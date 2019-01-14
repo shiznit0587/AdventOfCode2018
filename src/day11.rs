75fa3501ca7b965b0eed17c24ab6bbf36e8c8c12
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-14 11:45:57 -0500

    Changed Day 11 Sums to a struct with indexing traits

--- src/day11.rs
+++ src/day11.rs
@@ -13,7 +13,7 @@ pub fn day11(lines: &mut Vec<String>) {
     }
 
     let sums = build_sums(&grid, 3);
-    let winner = find_winner(&sums, 3);
+    let winner = sums.find_winner();
 
     println!(
         "Largest Power 3x3 anchor = {:?}, power = {}",
@@ -24,11 +24,11 @@ pub fn day11(lines: &mut Vec<String>) {
 
     let mut patch_size_winner: [Option<Identifier>; GRID_SIZE] = [None; GRID_SIZE];
     let mut sums: Sums = build_sums(&grid, 1);
-    patch_size_winner[0] = Some(find_winner(&sums, 1));
+    patch_size_winner[0] = Some(sums.find_winner());
 
     for patch_size in 2..GRID_SIZE + 1 {
-        sums = iter_sums(&grid, &sums, patch_size);
-        patch_size_winner[patch_size - 1] = Some(find_winner(&sums, patch_size));
+        sums = iter_sums(&grid, &sums);
+        patch_size_winner[patch_size - 1] = Some(sums.find_winner());
     }
 
     let winner = patch_size_winner
@@ -51,11 +51,10 @@ fn calc_power(x: i32, y: i32, serial: i32) -> i32 {
 }
 
 fn build_sums(grid: &Grid, patch_size: usize) -> Sums {
-    let sums_width = GRID_SIZE - patch_size + 1;
-    let mut sums = vec![0; sums_width * sums_width];
+    let mut sums = Sums::new(patch_size);
 
-    for coord in iproduct!(0..sums_width, 0..sums_width) {
-        sums[coord.0 * sums_width + coord.1] = iproduct!(0..patch_size, 0..patch_size)
+    for coord in iproduct!(0..sums.width, 0..sums.width) {
+        sums[coord.0][coord.1] = iproduct!(0..patch_size, 0..patch_size)
             .map(|c| grid[coord.0 + c.0][coord.1 + c.1])
             .sum();
     }
@@ -63,16 +62,15 @@ fn build_sums(grid: &Grid, patch_size: usize) -> Sums {
     sums
 }
 
-fn iter_sums(grid: &Grid, prev_sums: &Sums, patch_size: usize) -> Sums {
-    let sums_width = GRID_SIZE - patch_size + 1;
-    let mut sums = vec![0; sums_width * sums_width];
+fn iter_sums(grid: &Grid, prev_sums: &Sums) -> Sums {
+    let mut sums = Sums::new(prev_sums.patch_size + 1);
 
-    for coord in iproduct!(0..sums_width, 0..sums_width) {
+    for coord in iproduct!(0..sums.width, 0..sums.width) {
         // Get a collection of coords that are the edges to add to the previous sum
-        let mut edge_coords: Vec<Point> = Vec::with_capacity(patch_size * 2);
-        for i in 0..patch_size {
-            edge_coords.push((coord.0 + patch_size - 1, coord.1 + i));
-            edge_coords.push((coord.0 + i, coord.1 + patch_size - 1));
+        let mut edge_coords: Vec<Point> = Vec::with_capacity(sums.patch_size * 2);
+        for i in 0..sums.patch_size {
+            edge_coords.push((coord.0 + sums.patch_size - 1, coord.1 + i));
+            edge_coords.push((coord.0 + i, coord.1 + sums.patch_size - 1));
         }
 
         // Remove the duplicate corner
@@ -82,31 +80,11 @@ fn iter_sums(grid: &Grid, prev_sums: &Sums, patch_size: usize) -> Sums {
         let edges_sum: i32 = edge_coords.iter().map(|c| grid[c.0][c.1]).sum();
 
         // Track the iterative sum
-        let prev_sum = prev_sums[coord.0 * (sums_width + 1) + coord.1];
-        sums[coord.0 * sums_width + coord.1] = prev_sum + edges_sum;
+        let prev_sum = prev_sums[coord.0][coord.1];
+        sums[coord.0][coord.1] = prev_sum + edges_sum;
     }
 
     sums
-}
-
-fn find_winner(sums: &Sums, patch_size: usize) -> Identifier {
-    let sums_width = GRID_SIZE - patch_size + 1;
-
-    iproduct!(0..sums_width, 0..sums_width)
-        .map(|c| (c, sums[c.0 * sums_width + c.1]))
-        .max_by(|a, b| a.1.cmp(&b.1))
-        .unwrap()
-}
-
-fn _print_sums(sums: &Sums, patch_size: usize) {
-    println!("Sums for Patch Size = {}:", patch_size);
-    let sums_width = GRID_SIZE - patch_size + 1;
-    for j in 0..sums_width {
-        let line = (0..sums_width)
-            .map(|i| format!("{:3} ", sums[i * sums_width + j]))
-            .collect::<String>();
-        println!("{}", line);
-    }
 }
 
 const GRID_SIZE: usize = 300;
@@ -114,4 +92,51 @@ const GRID_SIZE: usize = 300;
 type Point = (usize, usize);
 type Identifier = (Point, i32);
 type Grid = [[i32; GRID_SIZE]; GRID_SIZE];
-type Sums = Vec<i32>;
+
+struct Sums {
+    sums: Vec<i32>,
+    patch_size: usize,
+    width: usize,
+}
+
+impl Sums {
+    fn new(patch_size: usize) -> Self {
+        let width = GRID_SIZE - patch_size + 1;
+        Sums {
+            sums: vec![0; width * width],
+            patch_size: patch_size,
+            width: width,
+        }
+    }
+
+    fn find_winner(&self) -> Identifier {
+        iproduct!(0..self.width, 0..self.width)
+            .map(|c| (c, self[c.0][c.1]))
+            .max_by(|a, b| a.1.cmp(&b.1))
+            .unwrap()
+    }
+
+    fn _print(&self) {
+        println!("Sums for Patch Size = {}:", self.patch_size);
+        for j in 0..self.width {
+            let line = (0..self.width)
+                .map(|i| format!("{:3} ", self[i][j]))
+                .collect::<String>();
+            println!("{}", line);
+        }
+    }
+}
+
+impl std::ops::Index<usize> for Sums {
+    type Output = [i32];
+
+    fn index(&self, i: usize) -> &[i32] {
+        &self.sums[(i * self.width)..((i + 1) * self.width)]
+    }
+}
+
+impl std::ops::IndexMut<usize> for Sums {
+    fn index_mut(&mut self, i: usize) -> &mut [i32] {
+        &mut self.sums[(i * self.width)..((i + 1) * self.width)]
+    }
+}
