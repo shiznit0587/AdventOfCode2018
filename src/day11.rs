70b9866aaecb9990299a3de4ed60a96c8ccd80f1
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-13 21:45:32 -0500

    Generalized the logic from Day 11 Part 1 for any patch size

--- src/day11.rs
+++ src/day11.rs
@@ -10,18 +10,8 @@ pub fn day11(lines: &mut Vec<String>) {
         grid[c.0][c.1] = calc_power(c.0 as i32, c.1 as i32, serial);
     }
 
-    let mut sums: [[i32; 298]; 298] = [[0; 298]; 298];
-
-    for coord in iproduct!(0..298, 0..298) {
-        sums[coord.0][coord.1] = iproduct!(0..3, 0..3)
-            .map(|c| grid[coord.0 + c.0][coord.1 + c.1])
-            .sum();
-    }
-
-    let winner = iproduct!(0..298, 0..298)
-        .map(|c| (c, sums[c.0][c.1]))
-        .max_by(|a, b| a.1.cmp(&b.1))
-        .unwrap();
+    let sums = build_sums(&grid, 3);
+    let winner = find_winner(&sums, 3);
 
     println!(
         "Largest Power 3x3 anchor = {:?}, power = {}",
@@ -33,4 +23,26 @@ pub fn day11(lines: &mut Vec<String>) {
 
 fn calc_power(x: i32, y: i32, serial: i32) -> i32 {
     ((((x + 10) * y + serial) * (x + 10)) / 100) % 10 - 5
+}
+
+fn build_sums(grid: &[[i32; 300]; 300], patch_size: u32) -> Vec<i32> {
+    let sums_width = 300 - patch_size + 1;
+    let mut sums = vec![0; (sums_width * sums_width) as usize];
+
+    for coord in iproduct!(0..sums_width, 0..sums_width) {
+        sums[(coord.0 * sums_width + coord.1) as usize] = iproduct!(0..3, 0..3)
+            .map(|c| grid[(coord.0 + c.0) as usize][(coord.1 + c.1) as usize])
+            .sum();
+    }
+
+    sums
+}
+
+fn find_winner(sums: &Vec<i32>, patch_size: u32) -> ((u32, u32), i32) {
+    let sums_width = 300 - patch_size + 1;
+
+    iproduct!(0..sums_width, 0..sums_width)
+        .map(|c| (c, sums[(c.0 * sums_width + c.1) as usize]))
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .unwrap()
 }
