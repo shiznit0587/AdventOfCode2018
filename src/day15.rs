5f2ba9e6c261acf0136fdd7934a3e9a0fe612ce8
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-01-21 19:08:46 -0500

    Finished movement logic for Day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,9 @@
+use itertools::Itertools;
+use pathfinding::directed::astar;
+use pathfinding::utils::absdiff;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
 pub fn day15(lines: &mut Vec<String>) {
     println!("Running Day 15 - a");
 
@@ -10,59 +16,49 @@ pub fn day15(lines: &mut Vec<String>) {
     while !ended {
         let mut queue = build_queue(&map);
 
-        for entry in queue {
-            // check if we're next to an enemy
-            // If we are, we don't move.
-            // Find all enemy units (targets)
-            let targets = (match map[entry] {
-                Tile::Goblin(_) => &map.elves,
-                Tile::Elf(_) => &map.goblins,
-                _ => panic!(),
-            })
-            .iter()
-            .filter(|u| u.alive)
-            .collect::<Vec<&Unit>>();
+        while let Some(mut entry) = queue.pop() {
+            let targets = find_targets(entry, &map);
 
-            // If any targets are already in range, don't move.
-
-            // I think I can do that by getting the locations adjacent to the target, and
-            // determining if entry's current location is in that list.
-            let mut target_points = Vec::new();
-            for t in targets.iter() {
-                let mut points = Vec::new();
-                if t.location.1 > 0 {
-                    points.push((t.location.0, t.location.1 - 1));
-                }
-                if t.location.0 > 0 {
-                    points.push((t.location.0 - 1, t.location.1));
-                }
-                if t.location.0 + 1 < map.width() {
-                    points.push((t.location.0 + 1, t.location.1));
-                }
-                if t.location.1 + 1 < map.height() {
-                    points.push((t.location.0, t.location.1 + 1));
-                }
-                for p in points {
-                    if map[p].is_open() {
-                        target_points.push(p);
-                    }
-                }
+            if targets.is_empty() {
+                ended = true;
+                break;
             }
 
-            if target_points.contains(&(entry.0, entry.1)) {
-                // Don't move, you're already in a target point
+            let target_locations: Vec<Point> = targets.iter().map(|t| t.location).collect();
+            let adjacent = get_neighbors(entry, &map)
+                .iter()
+                .find(|p| target_locations.contains(&p))
+                .is_some();
+
+            if !adjacent {
+                let target_points = find_target_points(targets, &map);
+
+                if target_points.is_empty() {
+                    continue;
+                }
+
+                let dest = find_destination(entry, &target_points, &map);
+                if dest.is_some() {
+                    map.swap(entry, dest.unwrap());
+                    entry = dest.unwrap();
+                }
             }
         }
 
+        map._print();
+
         round += 1;
-        ended = true;
+        ended = round > 3;
     }
+
+    println!("Combat finished after {} rounds", round);
 
     println!("Running Day 15 - b");
 }
 
-fn build_queue(map: &Map) -> Vec<Point> {
-    let mut queue = Vec::new();
+// Build a queue of points relating to all remaining units, in read order.
+fn build_queue(map: &Map) -> BinaryHeap<Point> {
+    let mut queue = BinaryHeap::new();
     for p in iproduct!(0..map.width(), 0..map.height()) {
         match map[p] {
             Tile::Goblin(_) | Tile::Elf(_) => queue.push(Point(p.0, p.1)),
@@ -70,6 +66,87 @@ fn build_queue(map: &Map) -> Vec<Point> {
         }
     }
     queue
+}
+
+// Find all remaining target units of the opposite type as the one located at start.
+fn find_targets(start: Point, map: &Map) -> Vec<&Unit> {
+    (match map[start] {
+        Tile::Goblin(_) => &map.elves,
+        Tile::Elf(_) => &map.goblins,
+        _ => panic!(),
+    })
+    .iter()
+    .filter(|u| u.alive)
+    .collect()
+}
+
+// Find all open points adjacent to a target.
+// Result is a BinaryHeap of unique Points sorted in read order.
+fn find_target_points(targets: Vec<&Unit>, map: &Map) -> BinaryHeap<Point> {
+    targets
+        .iter()
+        .flat_map(|t| get_open_neighbors(t.location, &map))
+        .unique()
+        .collect()
+}
+
+// Find a adjacent destination Point to move a unit from start to that is one step closer,
+// in read order, to the closest reachable target, in read order.
+fn find_destination(start: Point, target_points: &BinaryHeap<Point>, map: &Map) -> Option<Point> {
+    let choices: Vec<(usize, Point)> = target_points
+        .iter()
+        .filter_map(|tp| {
+            let a_stars = astar::astar_bag_collect(
+                &start,
+                |&p| get_open_neighbors(p, &map).into_iter().map(|p| (p, 1)),
+                |p| calc_manhattan(p, &tp),
+                |p| *p == *tp,
+            );
+
+            match a_stars {
+                Some(a_star_info) => {
+                    let mut paths = a_star_info.0;
+                    paths.sort_by(|a, b| b[1].cmp(&a[1]));
+                    Some((a_star_info.1, paths[0][1]))
+                }
+                None => None,
+            }
+        })
+        .sorted_by(|a, b| a.0.cmp(&b.0).then(b.1.cmp(&a.1)))
+        .collect();
+
+    match choices.first() {
+        Some(c) => Some(c.1),
+        None => None,
+    }
+}
+
+fn get_neighbors(p: Point, map: &Map) -> Vec<Point> {
+    let mut points = Vec::new();
+    if p.1 > 0 {
+        points.push(Point(p.0, p.1 - 1));
+    }
+    if p.0 > 0 {
+        points.push(Point(p.0 - 1, p.1));
+    }
+    if p.0 + 1 < map.width() {
+        points.push(Point(p.0 + 1, p.1));
+    }
+    if p.1 + 1 < map.height() {
+        points.push(Point(p.0, p.1 + 1));
+    }
+    points
+}
+
+fn get_open_neighbors(p: Point, map: &Map) -> Vec<Point> {
+    get_neighbors(p, &map)
+        .into_iter()
+        .filter(|i| map[*i].is_open())
+        .collect()
+}
+
+fn calc_manhattan(a: &Point, b: &Point) -> usize {
+    absdiff(a.0, b.0) + absdiff(a.1, b.1)
 }
 
 #[derive(Clone)]
@@ -130,8 +207,20 @@ impl Tile {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 struct Point(usize, usize);
+
+impl Ord for Point {
+    fn cmp(&self, other: &Point) -> Ordering {
+        other.1.cmp(&self.1).then(other.0.cmp(&self.0))
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Point) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 type Tiles = Vec<Vec<Tile>>;
 
@@ -184,10 +273,30 @@ impl Map {
         self.tiles.len()
     }
 
+    fn swap(&mut self, a: Point, b: Point) {
+        let tile_a = self[a].clone();
+        let tile_b = self[b].clone();
+        self.tiles[a.1][a.0] = tile_b;
+        self.tiles[b.1][b.0] = tile_a;
+
+        match self[a] {
+            Tile::Goblin(id) => self.goblins[id].location = a,
+            Tile::Elf(id) => self.elves[id].location = a,
+            _ => {}
+        }
+
+        match self[b] {
+            Tile::Goblin(id) => self.goblins[id].location = b,
+            Tile::Elf(id) => self.elves[id].location = b,
+            _ => {}
+        }
+    }
+
     fn _print(&self) {
         for line in self.tiles.iter() {
             println!("{}", line.iter().map(|t| t._to_char()).collect::<String>());
         }
+        println!();
     }
 }
 
@@ -222,16 +331,5 @@ impl Unit {
             hp: 300,
             alive: true,
         }
-    }
-
-    fn identify_targets(&self, map: &Map) -> Vec<Tile> {
-        match map.tiles[self.location.0][self.location.1] {
-            Tile::Elf(_) => {
-                // Get all remaining Goblins
-                // I could also do this on map, based on 'alive' units
-            }
-            _ => {}
-        }
-        Vec::new()
     }
 }
