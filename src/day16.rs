76bcb6fcaaf6f89be6a3edb7eca25bc4d9b84da7
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2019-02-11 11:00:12 -0500

    Finish Day 16 Part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,6 +1,7 @@
 use crate::utils;
 use itertools::Itertools;
 use regex::Regex;
+use std::collections::HashMap;
 
 pub fn day16(lines: &mut Vec<String>) {
     println!("Running Day 16 - a");
@@ -14,52 +15,107 @@ pub fn day16(lines: &mut Vec<String>) {
     }
 
     let input1 = &lines[..split + 1];
-    // let input2 = &lines[split + 3..];
+    let input2 = &lines[split + 3..];
 
     let rex = Regex::new(r"....... \[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
 
     let mut result = 0;
+    let mut samples: Vec<(usize, Vec<Op>)> = Vec::new();
+
     let mut iter = input1.iter().take(split + 1);
     while let Some((before, instruction, after, _)) = iter.next_tuple::<(_, _, _, _)>() {
-        let c = rex.captures(before).unwrap();
-        let before = [
-            utils::parse::<usize>(&c[1]),
-            utils::parse::<usize>(&c[2]),
-            utils::parse::<usize>(&c[3]),
-            utils::parse::<usize>(&c[4]),
-        ];
+        let before = parse_registers(before, &rex);
+        let instruction = parse_instruction(&instruction);
+        let after = parse_registers(after, &rex);
 
-        let i = instruction
-            .split(' ')
-            .map(|n| utils::parse::<usize>(&n))
-            .collect_vec();
-        let instruction = [i[0], i[1], i[2], i[3]];
+        let behaving_ops = test_ops(&before, &instruction, &after);
 
-        let c = rex.captures(after).unwrap();
-        let after = [
-            utils::parse::<usize>(&c[1]),
-            utils::parse::<usize>(&c[2]),
-            utils::parse::<usize>(&c[3]),
-            utils::parse::<usize>(&c[4]),
-        ];
-
-        let count = Op::iter()
-            .map(|op| (op, before.clone()))
-            .map(|(op, mut r)| {
-                op.get_fn()(&mut r, &instruction);
-                r
-            })
-            .filter(|r| *r == after)
-            .count();
-
-        if count >= 3 {
+        if behaving_ops.len() >= 3 {
             result += 1;
         }
+
+        samples.push((instruction[0], behaving_ops));
     }
 
     println!("Samples behaving as 3+ opcodes = {}", result);
 
     println!("Running Day 16 - b");
+
+    let op_codes = deduce_op_codes(&mut samples);
+
+    let mut r = [0, 0, 0, 0];
+    for instruction in input2 {
+        let i = parse_instruction(instruction);
+        op_codes[&i[0]].get_fn()(&mut r, &i);
+    }
+
+    println!("Program Results = {:?}", r);
+}
+
+fn parse_registers(registers: &String, rex: &Regex) -> [usize; 4] {
+    let c = rex.captures(registers).unwrap();
+    [
+        utils::parse::<usize>(&c[1]),
+        utils::parse::<usize>(&c[2]),
+        utils::parse::<usize>(&c[3]),
+        utils::parse::<usize>(&c[4]),
+    ]
+}
+
+fn parse_instruction(instructions: &String) -> [usize; 4] {
+    let i = instructions
+        .split(' ')
+        .map(|n| utils::parse::<usize>(&n))
+        .collect_vec();
+    [i[0], i[1], i[2], i[3]]
+}
+
+fn test_ops(before: &Registers, instruction: &Instruction, after: &Registers) -> Vec<Op> {
+    Op::iter()
+        .map(|op| (op, before.clone()))
+        .map(|(op, mut r)| {
+            op.get_fn()(&mut r, &instruction);
+            (op, r)
+        })
+        .filter(|(_, r)| r == after)
+        .map(|(op, _)| *op)
+        .collect_vec()
+}
+
+fn deduce_op_codes(samples: &mut Vec<(usize, Vec<Op>)>) -> HashMap<usize, Op> {
+    let mut op_codes = HashMap::new();
+
+    loop {
+        let mut found = None;
+
+        for entry in samples.iter() {
+            if entry.1.len() == 1 {
+                found = Some((entry.0, entry.1[0]));
+                // println!("Code {:?}", found);
+                break;
+            }
+        }
+
+        match found {
+            Some((op_code, op)) => {
+                op_codes.insert(op_code, op);
+
+                for entry in samples.iter_mut() {
+                    if entry.0 == op_code {
+                        entry.1 = Vec::new();
+                    } else {
+                        let idx = entry.1.iter().position(|&o| o == op);
+                        if idx.is_some() {
+                            entry.1.remove(idx.unwrap());
+                        }
+                    }
+                }
+            }
+            None => break,
+        }
+    }
+
+    op_codes
 }
 
 fn addr(r: &mut Registers, i: &Instruction) {
@@ -139,6 +195,7 @@ type Registers = [usize; 4];
 type Instruction = Registers;
 
 #[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Op {
     addr,
     addi,
